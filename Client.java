5fd5359e45dec89ead6d65f6098ac7e05c47cb82
karlpaolo <30851353+karlpaolo@users.noreply.github.com>
2019-12-06 08:17:11 +0800

    Update Client.java

--- Client.java
+++ Client.java
@@ -6,11 +6,14 @@ import java.util.*;
 import java.io.IOException;
 
 public class Client{
-	private static final String SERVER_IP = "127.0.0.1";
+	//private static final String SERVER_IP = "127.0.0.1";
 	private static final int SERVER_PORT = 3002;
 
 	public static void main(String[] args) throws IOException {
-		Socket socket = new Socket(SERVER_IP,SERVER_PORT);
+		
+		String serverName = args[0];
+		Socket socket = new Socket(serverName,SERVER_PORT);
+		//Socket socket = new Socket(SERVER_IP,SERVER_PORT);
 		BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 		PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
 		BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
